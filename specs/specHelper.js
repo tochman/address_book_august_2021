e6c9e4e7e723e394c7d3c698d64f96b63fcbb9c2
Thomas <thomas@craftacademy.se>
2021-08-18 17:08:26 +0200

    adds localStorage config to specHelper adds chai and sinon config to global

---
+++ specs/specHelper.js
@@ -0,0 +1,29 @@
+const { use, expect } = require("chai");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+use(sinonChai);
+
+global.expect = expect;
+global.sinon = sinon;
+
+global.window = {};
+
+global.window.localStorage = {
+  data: {},
+  clear() {
+    this.data = {};
+  },
+  setItem(key, value) {
+    this.data[key] = value;
+  },
+  getItem(key) {
+    const value = this.data[key];
+    if (value) {
+      return value;
+    }
+    return null;
+  },
+  removeItem(key) {
+    delete this.data[key];
+  },
+};
